841408159214fc021c116d0e15acc68a4c647838
Josu-A <aginagajosu@gmail.com>
2023-11-11 22:40:49 +0100

    Implement method that returns Events in an ExtendedIterator interface

--- src/main/java/businessLogic/BLFacadeImplementation.java
+++ src/main/java/businessLogic/BLFacadeImplementation.java
@@ -112,6 +112,20 @@ public class BLFacadeImplementation implements BLFacade {
 		dbManager.close();
 		return events;
 	}
+	
+	/**
+	 * This method invokes the data access to retrieve the events of a given date
+	 * 
+	 * @param date in which events are retrieved
+	 * @return collection of events
+	 */
+	@WebMethod
+	public ExtendedIterator<Event> getEventsIterator(Date date) {
+		dbManager.open(false);
+		Vector<Event> events = dbManager.getEvents(date);
+		dbManager.close();
+		return new ExtendedIteratorEvents(events.toArray(new Event[0]));
+	}
 
 	/**
 	 * This method invokes the data access to retrieve the dates a month for which
